6a47a5f4626223df9a5f8561d62111215d454ac5
000 <000@000-PC>
2020-01-03 13:30:56 +0800

    12345

--- _test/src/_test/test.java
+++ _test/src/_test/test.java
@@ -4,13 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		String s = "abc";
 		int a = 10;
-		System.out.println("ddsfa");
-		System.out.println(s);
-		System.out.println("adsfjoipawe");
-		System.out.println(a);
-		
-		System.out.println("adfadsdassf4werweq");
-		System.out.println(a);
+		System.out.println(a+s);
 
 	}
 }
