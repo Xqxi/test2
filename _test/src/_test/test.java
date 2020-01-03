bc878cdc86306e69c647a4e6acb70f5ac4cca558
000 <000@000-PC>
2020-01-03 11:45:15 +0800

    1

--- _test/src/_test/test.java
+++ _test/src/_test/test.java
@@ -2,10 +2,7 @@ package _test;
 
 public class test {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		String s = "abc";
-=======
 		int a = 10;
->>>>>>> branch 'master' of https://github.com/Xqxi/test2
 	}
 }
